73f3098913d007a90066450af84075c46fc39836
staubracing <staubracing@gmail.com>
2024-10-24 20:21:54 -0500

    added fetch

--- DevelopYouselfProjects/Project_4_API_toUsers/script.js
+++ DevelopYouselfProjects/Project_4_API_toUsers/script.js
@@ -1,4 +1,22 @@
 // Project 4 API to get users from jsonplaceholder
 
+const usersList = document.getElementById('usersList');
+const searchInput = document.getElementById('searchInput');
+const resultDiv = document.getElementById('result');
 
-const users
+async function getUsersData() {
+  const url = `https://jsonplaceholder.typicode.com/users`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+const users = re;
